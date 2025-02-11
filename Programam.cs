fd62eb4aac0feac36af8d45c34dfcbf3fc1ded11
karlodante <xydarkodayx@gmail.com>
2025-02-11 16:12:30 -0400

    update mi profile

---
+++ Programam.cs
@@ -0,0 +1,23 @@
+﻿
+
+public class Programam{
+    
+    public static void Main(String [] args) {
+       Sayayin sayayin1=  new Sayayin( "goku", "kamehameha" ,"kayokien",44,"ultra instinto" );
+       sayayin1.MostrarInformacion();
+       
+       Sayayin sayayin2 = new Sayayin("veggeta" ,"resplandor final", "onda espanciva ",  48,"ultra ego");
+       sayayin2.MostrarInformacion();
+       
+       Sayayin sayayin3 = new Sayayin("gohan" ,"super kamehameha", "aumento de poder ", 28, "modo bestia");
+       sayayin3.MostrarInformacion();
+       
+       Sayayin sayayin4 = new Sayayin("trunks" ,"corte de luz", "sentido de ki  ", 48 ,"super sayajin rage " );
+       sayayin4.MostrarInformacion();
+
+       Sayayin sayayin5 = new Sayayin("caulifa" ,"cañon destructor ", "estallido de energia ", 19," super sayajin fase 2");
+       sayayin5.MostrarInformacion();
+       
+    
+    }
+}
