4463f9b9eb9e610a712d8424a68b02463e3688b7
santi7-hub <santicadima4@gmail.com>
2025-02-12 13:01:30 -0400

    add my code Perro

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
 ﻿
 public class Program {
     public static void Main(string[] args) {
-        Persona persona = new Persona(1, "Gaston", "Quelali", new DateTime(1980, 1, 1), "Calle 123");
-        persona.MostrarInformacion();
+
+
+       Perro perro1 = new Perro("Husky", "Bruce","Blanco",2,"Aullar"  );
+       perro1.MostrarInformacion();
+       Perro perro2 = new Perro("Golden", "Linda","Dorado",10,"Ladrar"  );
+       perro2.MostrarInformacion();
+       Perro perro3 = new Perro("PastorAleman", "Zeus","Negro",4,"Olfato"  );
+       perro3.MostrarInformacion();
+       Perro perro4 = new Perro("Doberman", "king","Cafe",5,"Vision"  );
+       perro4.MostrarInformacion();
+
+
+
+
+
     }
 }
