d4e75e7b7b88ec580a2af2f1502002db3eb7445c
GastonQuelali <gaston_q@hotmail.com>
2025-02-18 12:37:26 -0400

    Add Materia class

--- Program.cs
+++ Program.cs
@@ -2,7 +2,10 @@
 public class Program {
     public static void Main(string[] args) {
        Universidad objUcatec = new Universidad("UCATEC", new Administrativo(1, "Juan", "Perez", new DateTime(1980, 1, 1), "Av. 6 de Agosto", "Rector"));
-
+    
        objUcatec.MostrarInformacion();
+        Docente objDocente1 = new Docente(2, "Maria", "Lopez", new DateTime(1985, 2, 2), "Av. 6 de Agosto", "Matemáticas", "Licenciatura"); 
+        Materia objMateria1 = new Materia("Matemáticas", objDocente1, "Aula 1", "8:00 - 10:00", 4);
+        objMateria1.MostrarInformacion();
     }
 }
--- src/Docente.cs
+++ src/Docente.cs
@@ -2,18 +2,18 @@ public class Docente : Persona
 {
     public string Especialidad { get; set; }
     public string GradoAcademico { get; set; }
-    public string CursosAsignados { get; set; }
+   
 
-    public Docente(int id, string nombres, string apellidos, DateTime fechaNacimiento, string direccion, string especialidad, string gradoAcademico, string cursosAsignados) : base(id, nombres, apellidos, fechaNacimiento, direccion) {
+    public Docente(int id, string nombres, string apellidos, DateTime fechaNacimiento, string direccion, string especialidad, string gradoAcademico): base(id, nombres, apellidos, fechaNacimiento, direccion) {
         Especialidad = especialidad;
         GradoAcademico = gradoAcademico;
-        CursosAsignados = cursosAsignados;
+   
     }
 
     public new void MostrarInformacion() {
         base.MostrarInformacion();
         Console.WriteLine($"Especialidad: {Especialidad}");
         Console.WriteLine($"Grado Académico: {GradoAcademico}");
-        Console.WriteLine($"Cursos Asignados: {CursosAsignados}");
+     
     }
 }
